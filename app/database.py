16f2cd38f5e1b9aaa5f5a7c9a9fd5af5b4f9b2c7
Hillol Das Pias <hillol4958@gmail.com>
2026-07-29 13:56:25 +0600

    Prep for deployment

--- app/database.py
+++ app/database.py
@@ -2,7 +2,11 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 from app.config import settings
 
-engine = create_async_engine(settings.database_url, echo=True)
+connect_args = {"ssl": "require"} if "neon.tech" in settings.database_url else {}
+
+engine = create_async_engine(
+    settings.database_url, echo=True, connect_args=connect_args
+)
 
 SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
